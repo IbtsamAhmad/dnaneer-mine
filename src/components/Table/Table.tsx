ed27ba10bbf59ae7b9e0c72bac2ecbaf25712568
Ibtsam Ahmad <ibtsamahmad01@gmail.com>
2023-04-19 17:55:36 +0500

    table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import "./Table.scss"
+// import React from 'react'
+// import "./Table.scss"
 
-interface TableProps = {
-  style?: React.CSSProperties;
-  columns: any;
-  data:any
+// interface TableProps = {
+//   style?: React.CSSProperties;
+//   columns: any;
+//   data:any
+// }
+
+// const AppTable = ({
+//   children,
+//   className,
+//   style
+//   data,
+//   columns
+// }: MessageProps): JSX.Element => {
+//   return <Table columns={columns} dataSource={data} />;
+// }
+
+// export default AppTable;
+
+
+const Table = () => {
+  return (
+    <div>Table</div>
+  )
 }
 
-const AppTable = ({
-  children,
-  className,
-  style
-  data,
-  columns
-}: MessageProps): JSX.Element => {
-  return <Table columns={columns} dataSource={data} />;
-}
-
-export default AppTable;
+export default Table
