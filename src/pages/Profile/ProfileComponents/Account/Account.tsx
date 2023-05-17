db9524f160e5633199d02836ea4676bd52461062
Ibtsam Ahmad <ibtsamahmad01@gmail.com>
2023-05-18 00:24:24 +0500

    de activaet model

--- src/pages/Profile/ProfileComponents/Account/Account.tsx
+++ src/pages/Profile/ProfileComponents/Account/Account.tsx
@@ -1,19 +1,119 @@
-import { useState } from "react";
-import Button from "components/Button/Button";
+import { useState, useEffect } from "react";
+
 import { Row, Col, Form, Input } from "antd";
+import Modal from "components/Modal/Modal";
+import Button from "components/Button/Button";
+import Otp from "components/OTP/Otp";
+import { timeConverter } from "utils/Helper";
 import { ReactComponent as EditProfile } from "assets/svgs/EditProfile.svg";
+import { ReactComponent as DeActivate } from "assets/svgs/DeActivate.svg";
+
 import "./Account.scss";
 
 const AccountContent = ({}) => {
   const [editAccount, setEditAccount] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [time, setTime] = useState<number>(60);
+  const [confirmDeactivateModal, setConfirmDeactivateModal] = useState(false)
+
+  useEffect(() => {
+    let timer;
+    if (time !== 0) {
+      timer = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [time]);
+
   const onFinish = (values) => {
     console.log("Success:", values);
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const confirmDeactivate = () => {
+    if (otp === "1234") {
+      setIsModalOpen(false);
+      setConfirmDeactivateModal(true)
+    }
+  };
+    const handleConfirmCancel = () => {
+      setConfirmDeactivateModal(false);
+    };
+    const confirmAccountDelete = () =>{
+       setConfirmDeactivateModal(false);
+    }
+
   return (
     <div className="account-panel-container">
+      <Modal
+        className="deactivate-modal"
+        isModalVisible={confirmDeactivateModal}
+        onOk={confirmAccountDelete}
+        onCancel={handleConfirmCancel}
+        footer={false}
+      >
+        <div className="icon">
+          <DeActivate />
+        </div>
+
+        <h1>Deactivate your account?</h1>
+        <p className="cancel" style={{ marginTop: "12px", fontSize: "14px" }}>
+          Are you sure you want to deactivate?
+        </p>
+
+        <Button
+          onClick={confirmAccountDelete}
+          block
+          className="confirm-deactivate-btn"
+          style={{ marginTop: "35px" }}
+        >
+          Deactivate
+        </Button>
+        <p className="cancel" onClick={() => handleConfirmCancel()}>
+          Cancel
+        </p>
+      </Modal>
+
+      <Modal
+        className="deactivate-modal"
+        isModalVisible={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        <h2>Please type the OTP received on your phone</h2>
+        <div className="deactivate-otp-container">
+          <Otp setOtp={setOtp} otp={otp} />
+        </div>
+        <p className="timer">
+          {time !== 0 ? timeConverter(time) : "OTP expired"}{" "}
+          {time ? <span>left</span> : null}
+        </p>
+        <Button
+          onClick={confirmDeactivate}
+          block
+          className="confirm-deactivate-btn"
+        >
+          Confirm deactivation
+        </Button>
+        <p className="cancel" onClick={() => handleCancel()}>
+          Cancel
+        </p>
+      </Modal>
       <div className="account-row">
         <h1>Upgrade to VIP Investor</h1>
         <Button className="upgrade-btn">Upgrade</Button>
@@ -164,7 +264,9 @@ const AccountContent = ({}) => {
       </div>
       <div className="deactivate-row">
         <p>Deactivate my account</p>
-        <Button className="deactivate-btn">Deactivate</Button>
+        <Button className="deactivate-btn" onClick={() => showModal()}>
+          Deactivate
+        </Button>
       </div>
     </div>
   );
